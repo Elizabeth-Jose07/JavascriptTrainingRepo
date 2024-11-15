a87f6eb3e01fd18ab06988d14d5fffb46c2c3f80
Elizabeth Jose <elzalizjose@gmail.com>
2024-11-15 15:28:34 +0530

    chnages

--- scientificCalculations.js
+++ scientificCalculations.js
@@ -1,16 +1,26 @@
 function scientificCalculation() {
-  const gravitaionalConstant = (6.6743 * 10) ** -11;
-  const planksConstant = (6.62607015 * 10) ** -34;
+  const gravitationalConstant = (6.6743 * 10) ** (-11);
+  const planksConstant = (6.62607015 * 10) ** (-34);
   const speedLight = 299792458;
-  const energyPhoton = (5 * 10) ** 14;
 
+  let frequency = (5 * 10) ** 14;
   let distance = 1;
   let objectA = 1;
   let objectB = 1;
 
-  let gravitaionalForce = (gravitaionalConstant * objectA * objectB) / distance;
+  let gravitaionalForce = (gravitationalConstant * objectA * objectB) /distance;
+  let g = gravitaionalForce.toFixed(5);
+  //gravitaionalForce = gravitaionalForce.toFixed(5);
 
-  return gravitaionalForce;
+  let energyPhoton = (planksConstant * frequency);
+  let e = energyPhoton.toFixed(5);
+  //energyPhoton = energyPhoton.toFixed(5);
+  
+  return {
+    gravitaionalForce: g,
+    energyPhoton: e,
+  };
 }
+const result = scientificCalculation();
 
-console.log(scientificCalculation());
+console.log("Gravitational force = "+result.gravitaionalForce+" energy of photon = "+result.energyPhoton);
