1eb642380a518d0fcf0a9abd2ff218b8b9648980
Elizabeth Jose <elzalizjose@gmail.com>
2024-11-15 16:40:50 +0530

    changes

--- cryptoCurrency.js
+++ cryptoCurrency.js
@@ -1,6 +1,6 @@
 function cryptoCurrency() {
   const bitCoinRate = 35000;
-  const ethreumRate = 2000;
+  const ethereumRate = 2000;
   const dogeCoinRate = 0.005;
 
   let bitCoinAmount = 2;
@@ -8,7 +8,7 @@ function cryptoCurrency() {
   let dogeCoinAmount = 10000;
 
   let bitCoinTotal = bitCoinAmount * bitCoinRate;
-  let ethereumTotal = ethereumAmount * ethreumRate;
+  let ethereumTotal = ethereumAmount * ethereumRate;
   let dogeCoinTotal = dogeCoinAmount * dogeCoinRate;
 
   let totalInUSD = bitCoinTotal + ethereumTotal + dogeCoinTotal;
