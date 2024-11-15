31167a7a91af5a97df57883909d0b88c03a60f41
Elizabeth Jose <elzalizjose@gmail.com>
2024-11-15 11:52:27 +0530

    chnages in script

---
+++ playerGameScore.js
@@ -0,0 +1,11 @@
+class PlayerGameScore{
+   async scorCalculator(){
+     let score = 10;
+     score = score + 5;
+     score = score - 3
+     score = score * score;
+     score = score / 2;
+
+     console.log(score);
+   }
+}
