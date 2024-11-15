6cec866385345cc054b4c5d83b56d7dcaf941c8e
Elizabeth Jose <elzalizjose@gmail.com>
2024-11-15 12:22:45 +0530

    chnages

--- playerGameScore.js
+++ playerGameScore.js
@@ -1,11 +1,11 @@
-class PlayerGameScore{
-   async scorCalculator(){
-     let score = 10;
-     score +=5;
-     score -=3
-     score *=2
-     score /=2;
+function scoreCalculator(){
+  let score = 10;
+  score +=5;
+  score -=3;
+  score *=2
+  score /=2;
 
-     console.log(score);
-   }
+  return score;
 }
+
+console.log(scoreCalculator());
