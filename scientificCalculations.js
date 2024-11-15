15b77a704e341be4b0130d0e36b941f28de47678
Elizabeth Jose <elzalizjose@gmail.com>
2024-11-15 15:39:53 +0530

    changes

--- scientificCalculations.js
+++ scientificCalculations.js
@@ -1,24 +1,20 @@
 function scientificCalculation() {
-  const gravitationalConstant = (6.6743 * 10) ** (-11);
-  const planksConstant = (6.62607015 * 10) ** (-34);
-  const speedLight = 299792458;
+    const gravitationalConstant = 6.6743e-11;
+    const planksConstant = 6.62607015e-34;
+    const speedLight = 299792458;
+  
+    let frequency = 5e14;
+    let distance = 1;
+    let objectA = 1;
+    let objectB = 1;
 
-  let frequency = (5 * 10) ** 14;
-  let distance = 1;
-  let objectA = 1;
-  let objectB = 1;
-
-  let gravitaionalForce = (gravitationalConstant * objectA * objectB) /distance;
-  let g = gravitaionalForce.toFixed(5);
-  //gravitaionalForce = gravitaionalForce.toFixed(5);
-
+  let gravitaionalForce = (gravitationalConstant * objectA * objectB) /(distance *distance);
+  
   let energyPhoton = (planksConstant * frequency);
-  let e = energyPhoton.toFixed(5);
-  //energyPhoton = energyPhoton.toFixed(5);
   
   return {
-    gravitaionalForce: g,
-    energyPhoton: e,
+    gravitaionalForce: gravitaionalForce,
+    energyPhoton: energyPhoton,
   };
 }
 const result = scientificCalculation();
